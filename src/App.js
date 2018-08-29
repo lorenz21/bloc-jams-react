c0bbf16345dda954cfa9f6680dbc261ce5326ff2
Steve <lorenz21@hawaii.edu>
2018-08-29 13:31:29 -1000

    Checkpoint 10 styling: completed

--- src/App.js
+++ src/App.js
@@ -9,14 +9,35 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+          <header className="mdl-layout__header">
+            <div className="mdl-layout__header-row">
+
+            <span className="mdl-layout-title">Bloc Jams</span>
+
+            <div className="mdl-layout-spacer"></div>
+
+            <nav className="mdl-navigation mdl-layout--large-screen-only">
+              <Link to='/' className="mdl-navigation__link">Landing</Link>
+              <Link to='/library' className="mdl-navigation__link">Library</Link>
+            </nav>
+            </div>
+          </header>
+        <div className="mdl-layout__drawer">
+          <span className="mdl-layout-title">Bloc Jams</span>
+          <nav className="mdl-navigation">
+            <Link to='/' className="mdl-navigation__link">Landing</Link>
+            <Link to='/library' className="mdl-navigation__link">Library</Link>
+          </nav>
+        </div>
+      </div>
         <header>
           <nav>
             <Link to='/'>Landing</Link>
             <Link to='/library'>Library</Link>
           </nav>
-          <h1>Bloc Jams</h1>
         </header>
-        <main>
+        <main className="mdl-layout__content">
         <Route exact path="/" component={Landing} />
         <Route path="/library" component={Library} />
         <Route path="/album/:slug" component={Album} />
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Landing.css'
 
 const Landing = () => (
-  <section className="landing">
-    <h1 className="hero-title">Turn the music up!</h1>
-
-    <section className="selling-points">
+  <div className="mdl-grid">
+    <div className="mdl-cell mdl-cell--12-col">
+        <h1>Bloc Jams</h1>
+        <h2 className="hero-title">Turn the music up!</h2>
       <div className="point">
         <h2 className="point-title">Choose your music</h2>
         <p className="point-description">The world is full of music; why should you have to listen to music someone else chose?</p>
@@ -17,8 +18,8 @@ const Landing = () => (
         <h2 className="point-title">Mobile enabled</h2>
         <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
       </div>
-    </section>
-  </section>
+    </div>
+  </div>
 );
 
 export default Landing;
